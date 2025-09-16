e09610e3c6cf73b3cf712a0a0e7815952aca9552
GowshikM-2005 <gowshik0105@gmail.com>
2025-09-16 18:26:46 +0530

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -1,43 +1,39 @@
-def call(Map config = [:]){
+def call(Map config = [:]) {
     pipeline {
-        agent any 
-       stages {
-               stage('Checkout Code') {
+        agent any
+
+        stages {
+            stage('Checkout Code') {
                 steps {
                     git branch: config.get('gitBranch', 'main'),
                         url: config.get('gitRepo', 'https://github.com/GowshikM-2005/jenkins-shared-lib.git')
                 }
             }
-         // stage('Terraform Setup'){
-         //    steps {
-         //        sh """
-         //              curl -fsSL https://releases.hashicorp.com/terraform/\${TF_VERSION}/terraform_\${TF_VERSION}_linux_amd64.zip -o terraform.zip
-         //              unzip terraform.zip
-         //              sudo mv terraform /usr/local/bin/
-         //              terraform --version
-         //            """
-         //    }
-         // }
-        stage('Terraform Init'){
-            steps {
-                 withCredentials([file(credentialsId: 'kubeconfig-secret', variable: 'KUBECONFIG')]) {
-                sh 'terraform init'
-                 }       
+
+            stage('Terraform Init') {
+                steps {
+                    withCredentials([file(credentialsId: 'kubeconfig-secret', variable: 'KUBECONFIG')]) {
+                        sh 'terraform init'
+                    }
+                }
             }
-        }
-        stage('Terraform Plan'){
-            steps {
-                sh 'terraform plan -out=tfplan'
+
+            stage('Terraform Plan') {
+                steps {
+                    sh 'terraform plan -out=tfplan'
+                }
             }
-        }
-        stage('Terraform Apply'){
-            steps {
+
+            stage('Terraform Apply') {
                 when {
                     expression { return config.get('autoApprove', false) }
                 }
-                sh 'terraform apply -auto-approve tfplan'
-            }    
-       }
+                steps {
+                    withCredentials([file(credentialsId: 'kubeconfig-secret', variable: 'KUBECONFIG')]) {
+                        sh 'terraform apply -auto-approve'
+                    }
+                }
+            }
+        }
     }
-}
 }
